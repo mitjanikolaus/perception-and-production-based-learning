e15962eac82011554d822ba5531ac135a7f42389
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-03-05 11:45:04 +0100

    Update sbatch scripts

--- sbatch_scripts/train_captioning_sat.sh
+++ sbatch_scripts/train_captioning_sat.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=train-SAT
 #
 #SBATCH --ntasks=1
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=32000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -11,5 +11,5 @@
 #SBATCH --error=out/train_captioning_sat.out
 
 source activate egg
-python -u train_image_captioning.py --lr 0.0001 --n_epochs 10 --model show_attend_and_tell #--fine-tune-resnet
+python -u train_image_captioning.py --lr 0.0001 --n_epochs 15 --model show_attend_and_tell #--fine-tune-resnet
 
--- sbatch_scripts/train_ranking.sh
+++ sbatch_scripts/train_ranking.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=train-ranking
 #
 #SBATCH --ntasks=1
-#SBATCH --time=12:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=32000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=out/train_ranking.out
-#SBATCH --error=out/train_ranking.out
+#SBATCH --output=out/train_ranking_5.out
+#SBATCH --error=out/train_ranking_5.out
 
 source activate egg
-python -u train_image_sentence_ranking.py --lr 0.0001 --n_epochs 10
+python -u train_image_sentence_ranking.py --lr 0.0001 --n_epochs 15 --checkpoint-dir ~/data/visual_ref/checkpoints/ranking_run_5/
 
