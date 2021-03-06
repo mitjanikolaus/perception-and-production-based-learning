0e50173af48278c88afe66d9077f35c237156878
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-03-06 14:58:37 +0100

    Group noun accuracies in plot

--- plot_accuracies.py
+++ plot_accuracies.py
@@ -24,8 +24,22 @@ LEGEND = {
     "data/semantics_eval_semantic_roles_filtered.csv": "semantic roles",
 }
 
+LEGEND_GROUPED_NOUNS = {
+    "data/semantics_eval_persons.csv": "nouns",
+    "data/semantics_eval_animals.csv": "nouns",
+    "data/semantics_eval_inanimates.csv": "nouns",
+    "data/semantics_eval_verbs.csv": "verbs",
+    "data/semantics_eval_adjectives.csv": "adjectives",
+    "data/semantics_eval_adjective_noun_binding.csv": "adjective-noun dependency",
+    "data/semantics_eval_verb_noun_binding_filtered.csv": "verb-noun dependency",
+    "data/semantics_eval_semantic_roles_filtered.csv": "semantic roles",
+}
+
 
 def main(args):
+    sns.set_context("paper", rc={"font.size": 12, "axes.titlesize": 12, "axes.labelsize": 12, "xtick.labelsize": 12,
+                                 "ytick.labelsize": 12, "legend.fontsize":12})
+
     all_scores = []
     for run, scores_file in enumerate(args.scores_files):
         scores = pickle.load(open(scores_file, "rb"))
@@ -54,6 +68,11 @@ def main(args):
         scores.set_index("num_samples", inplace=True)
 
         scores.rename(columns=LEGEND, inplace=True)
+        if args.group_noun_accuracies:
+            scores.insert(0, "nouns", scores[["persons", "animals", "objects"]].mean(axis=1))
+            del scores["persons"]
+            del scores["animals"]
+            del scores["objects"]
 
         all_scores.append(scores.copy())
 
@@ -62,10 +81,11 @@ def main(args):
     sns.lineplot(data=all_scores, ci="sd")
 
     plt.xlim((0, args.x_lim))
+    plt.ylim((0.49, 0.86))
 
     # Add chance level line
     plt.axhline(y=0.5, color="black", label="Chance level", linestyle="--")
-    plt.text(450000, 0.505, 'Chance level', fontsize=10, va='center', ha='center')
+    plt.text(440000, 0.507, 'Chance level', fontsize=12, va='center', ha='center')
 
     plt.ylabel("Accuracy")
     plt.tight_layout()
@@ -82,6 +102,11 @@ def get_args():
     )
     parser.add_argument(
         "--x-lim", default=TRAINING_SET_SIZE*10, type=int,
+    )
+    parser.add_argument(
+        "--group-noun-accuracies",
+        default=False,
+        action="store_true",
     )
 
     return parser.parse_args()
