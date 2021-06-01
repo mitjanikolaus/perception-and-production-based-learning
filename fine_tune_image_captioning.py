77194b8b06d06fa8fbe151af611880385679ce53
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-06-01 21:28:04 +0200

    Refactoring

--- fine_tune_image_captioning.py
+++ fine_tune_image_captioning.py
@@ -48,32 +48,6 @@ from utils import (
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class MeanBaseline:
-    """Running mean baseline; all loss batches have equal importance/weight,
-    hence it is better if they are equally-sized.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-        self.mean_baseline = torch.zeros(1, requires_grad=False)
-        self.n_points = 0.0
-
-    def update(self, loss: torch.Tensor) -> None:
-        self.n_points += 1
-        if self.mean_baseline.device != loss.device:
-            self.mean_baseline = self.mean_baseline.to(loss.device)
-
-        self.mean_baseline += (
-            loss.detach().mean().item() - self.mean_baseline
-        ) / self.n_points
-
-    def predict(self, loss: torch.Tensor) -> torch.Tensor:
-        if self.mean_baseline.device != loss.device:
-            self.mean_baseline = self.mean_baseline.to(loss.device)
-        return self.mean_baseline
-
-
 def forward_pass_rl(model, images, captions, vocab, args):
     sequences, logits, entropies, sequence_lengths = model.decode(
         images, sampling=True
