8c6fff8c1819e1ea3ba235220ed7c6e22b3ccf7f
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-06-14 20:00:49 +0200

    Update sbatch scripts

--- sbatch_scripts/finetune_captioning_rl.sh
+++ sbatch_scripts/finetune_captioning_rl.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 ##SBATCHCOMMENT --constraint=cuda75|cuda61
-#SBATCH --output=out/fine_tune_captioning_st_trainset_frac_1.0_frequency_rl_updates_-1.out
+#SBATCH --output=out/fine_tune_captioning_st_trainset_frac_1.0_frequency_rl_updates_-1_log_stats.out
 
 source activate egg
-python -u train_image_captioning.py --seed 4 --lr 0.00001 --log-frequency 50 --n-epochs 5000 --model show_and_tell --fine-tune-resnet --out-checkpoints-dir results/xsl_then_rl/4/ --eval-semantics --frequency-rl-updates -1 --entropy-coeff 0 --checkpoint ~/nn_language_acquisition/visual_ref/results/xsl/4/show_and_tell_train_frac_1.0.pt --training-set-size 1.0
+python -u train_image_captioning.py --seed 1 --lr 0.00001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --fine-tune-resnet --out-checkpoints-dir results_stats/xsl_then_rl/1/ --eval-semantics --frequency-rl-updates -1 --entropy-coeff 0 --checkpoint ~/nn_language_acquisition/visual_ref/results/xsl/4/show_and_tell_train_frac_1.0.pt --training-set-size 1.0 --log-produced-utterances-stats
--- sbatch_scripts/train_captioning_st_alternate.sh
+++ sbatch_scripts/train_captioning_st_alternate.sh
@@ -8,9 +8,17 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=cuda75|cuda61
-#SBATCH --output=out/train_st_alternate_trainset_frac_1.0_frequency_rl_updates_1.out
+#SBATCH --output=out/train_st_alternate_trainset_frac_1.0_frequency_rl_updates_10.out
 
 source activate egg
-python -u train_image_captioning.py --seed 4 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate/4/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 1
+#python -u train_image_captioning.py --seed 7 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate_new/0/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 10
+
+#python -u train_image_captioning.py --seed 1 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate_new/1/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 10
+
+#python -u train_image_captioning.py --seed 2 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate_new/2/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 10
+
+#python -u train_image_captioning.py --seed 3 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate_new/3/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 10
+
+python -u train_image_captioning.py --seed 4 --lr 0.0001 --log-frequency 100 --n-epochs 5000 --model show_and_tell --out-checkpoints-dir results/alternate_new/4/ --training-set-size 1.0 --fine-tune-resnet --entropy-coeff 0 --eval-semantics --frequency-rl-updates 10
 
 
